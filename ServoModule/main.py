348b30a8888bb5a6b162fb897a9fa47e8c4171b2
YBQ <baoqyang@163.com>
2020-05-16 15:53:56 +0800

    Update main.py

--- ServoModule/main.py
+++ ServoModule/main.py
@@ -5,10 +5,10 @@ from Sensor import *
 
 
 # 主程序
-my_servo = Servo(9, 0)
-my_robot = Robot(my_servo)
+my_servo = Servo(9, 0)  # 将舵机初始化为引脚9，初始角度0°
+my_robot = Robot(my_servo)  # 用舵机实例初始化robot
 while True:
-    my_robot.begin()
-    my_robot.transport()
-    my_robot.end()
-    my_robot.back()
+    my_robot.begin()  # 装货阶段，连接货箱
+    my_robot.transport()  # 运输阶段
+    my_robot.end()  # 卸货阶段，断开连接
+    my_robot.back()  # 返回出发点，继续运输
